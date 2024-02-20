41e5e0cf0679bb54715449407106af4eb86774f0
“wahmx <oozryr@gmail.com>
2024-02-20 23:49:15 +0800

    update build

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ shopt -s dotglob
 # Vercel only does a shallow clone, which loses git info
 if [ "$VERCEL_ENV" = "production" ]; then
   rm -rf *
-  git clone --recursive -b $VERCEL_GIT_COMMIT_REF https://github.com/oozry12/asyihui.git .
+  git clone  https://github.com/oozry12/asyihui.git .
   git config advice.detachedHead false
-  git checkout $VERCEL_GIT_COMMIT_SHA
+  git checkout 
 fi
 
 # Netlify uses $CONTEXT and Vercel uses $VERCEL_ENV
