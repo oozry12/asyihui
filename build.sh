1852d77c63678637903ac716712a1f4e0fea4c8d
“wahmx <oozryr@gmail.com>
2024-02-20 23:47:30 +0800

    update build

--- build.sh
+++ build.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
 shopt -s dotglob
 
-# do not index the site on Netlify (only index the one built on Vercel)
-if [ "$NETLIFY" = "true" ]; then
-  cd static
-  echo 'User-agent: *
-Disallow: /' > robots.txt
-  cd ..
-fi
+
 
 # Vercel only does a shallow clone, which loses git info
 if [ "$VERCEL_ENV" = "production" ]; then
   rm -rf *
-  git clone --recursive -b $VERCEL_GIT_COMMIT_REF https://github.com/$VERCEL_GIT_REPO_OWNER/$VERCEL_GIT_REPO_SLUG.git .
+  git clone --recursive -b $VERCEL_GIT_COMMIT_REF https://github.com/oozry12/asyihui.git .
   git config advice.detachedHead false
   git checkout $VERCEL_GIT_COMMIT_SHA
 fi
